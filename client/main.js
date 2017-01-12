4f4c0ff23a2815ce386798165ad25fb9c848717f
yoblig <gilboyk@gmail.com>
2017-01-12 00:35:39 -0600

    basic CRUD working

--- client/main.js
+++ client/main.js
@@ -5,6 +5,35 @@ import './main.html';
 
 Template.main.helpers({
   todos: function(){
-    return Todos.find();
+    return Todos.find({}, {sort: {createdAt: -1}});
+  }
+});
+
+Template.main.events({
+  'submit .new-todo': function(event){
+    var text = event.target.text.value;
+    
+    Todos.insert({
+      title: text,
+      createdAt: new Date()
+    })
+
+    event.target.text.value = '';
+    return false;
+  },
+  'click .toggle-check': function(){
+    Todos.update(
+      this._id, 
+      { $set: 
+        {
+          checked: ! this.checked
+        }
+      }
+      );
+  },
+  'click .delete-todo': function(){
+    if(confirm("Are you sure?")){
+      Todos.remove(this._id);
+    }
   }
 });
